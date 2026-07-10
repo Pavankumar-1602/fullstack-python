8da41d6e235d17589bb2ef527b2b489246278d5d
pavan kumar <cspavankumar123@gmail.com>
2026-07-10 08:41:47 +0530

    day3.3

--- day3.py
+++ day3.py
@@ -1,103 +1,105 @@
-# #def add(a,b): 
-# # #     print(a+b)
-# # # add(10,20)
-# # # ########################3333
-# # # def student(**details):
-# # #     print(details)
+# # #def add(a,b): 
+# # # #     print(a+b)
+# # # # add(10,20)
+# # # # ########################3333
+# # # # def student(**details):
+# # # #     print(details)
     
     
-# # # ##############################
-# # # def square(x):
-# # #      return x*x
-# # # print(square(16))
+# # # # ##############################
+# # # # def square(x):
+# # # #      return x*x
+# # # # print(square(16))
     
-# # # ###########################################    
-# # # add = lambda a,b : a+b
-# # # print(add(10,50))
-# # # # ####################################
-# # # add = lambda a,b : a+b
-# # # print(add(10,50))
+# # # # ###########################################    
+# # # # add = lambda a,b : a+b
+# # # # print(add(10,50))
+# # # # # ####################################
+# # # # add = lambda a,b : a+b
+# # # # print(add(10,50))
 
-# # thima = lambda x:"Even" if x%2==0 else "Odd"
-# # # print(thima(10))
-# # # print(thima(11))
-
-
-
-# # ##############################################(upper)
-
-# # ding =lambda name :name .upper()
-# # print(ding("pavan"))
+# # # thima = lambda x:"Even" if x%2==0 else "Odd"
+# # # # print(thima(10))
+# # # # print(thima(11))
 
 
 
-# file = open("student.txt","w")
-# file.write("hello ")
-# file.close()
+# # # ##############################################(upper)
 
-# print("Data written successfully ")
+# # # ding =lambda name :name .upper()
+# # # print(ding("pavan"))
 
-# file = open("student.txt","r")
-# data = file.read()
-# print(data)
-# file.close()
 
-# file = open("student.txt","a")
-# file.write("hello students")
-# file.close()
 
-# print("Data appended successfully ")
+# # file = open("student.txt","w")
+# # file.write("hello ")
+# # file.close()
 
-# file = open("student.txt","r")
-# data = file.read()
-# file.close()
+# # print("Data written successfully ")
 
-#################################################################################################
+# # file = open("student.txt","r")
+# # data = file.read()
+# # print(data)
+# # file.close()
+
+# # file = open("student.txt","a")
+# # file.write("hello students")
+# # file.close()
+
+# # print("Data appended successfully ")
+
+# # file = open("student.txt","r")
+# # data = file.read()
+# # file.close()
+
+# #################################################################################################
+
+# # try:
+# #     a=10
+# #     b=0
+# #     print(a/b)
+# # except: 
+# #     print("something went wrong")    
+    
+# #################################################################################################
+
+# # try:
+# #     num = int(input("Enter a number: "))
+# #     print(num)
+# # except ValueError:
+# #     print("enter a valid number")
+    
+# # ###################################################################################################
+
+# # try:
+# #     a=  int(input("Enter a number a: "))
+# #     b=  int(input("Enter a number b: "))  
+# #     print(a/b)
+# # except ValueError:
+# #     print("enter valid numbers")
+# # except ZeroDivisionError:
+# # #     print("cannot divide by zero")
+    
+# # ###############################################################################################
+
+# # try:
+# #     file = open("data.txt",)
+# #     print(file.read())
+# # except:
+# #     print("file not found")
+# # finally:
+# #     print("program completed")
+        
+        
+        
+# ################################################################################################(else block)
 
 # try:
-#     a=10
-#     b=0
-#     print(a/b)
-# except: 
-#     print("something went wrong")    
-    
-#################################################################################################
+#     print(10/2)
 
-# try:
-#     num = int(input("Enter a number: "))
-#     print(num)
-# except ValueError:
-#     print("enter a valid number")
-    
-# ###################################################################################################
-
-# try:
-#     a=  int(input("Enter a number a: "))
-#     b=  int(input("Enter a number b: "))  
-#     print(a/b)
-# except ValueError:
-#     print("enter valid numbers")
-# except ZeroDivisionError:
-# #     print("cannot divide by zero")
-    
-# ###############################################################################################
-
-# try:
-#     file = open("data.txt",)
-#     print(file.read())
 # except:
-#     print("file not found")
-# finally:
-#     print("program completed")
-        
-        
-        
-################################################################################################(else block)
+#     print("error")
+# else:
+#     print("success")
 
-try:
-    print(10/2)
-
-except:
-    print("error")
-else:
-    print("success")
+#################################################################################################()
